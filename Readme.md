1c0a24acacc9660e523ae10433f6f8972a75d80d
Cal <cal@tripleanalytics.com>
2015-10-25 02:57:44 -0400

    Major progress on pre-processing
    Training data set is parsed & labeled. Working on separating feature
    data set & building data dictionary. Estimating an additional 2-3 hours
    of work left.

--- Readme.md
+++ Readme.md
@@ -8,3 +8,7 @@
 The original data files and acompanied readme & descriptions are in the folder UCI-HAR-Dataset.
 
 I ran the R script run-analysis.R to transform the data from the subfolders "train" & "test" in the original dataset to the folder "tidy"
+
+The original dataset was sourced from 
+
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,36 @@
-## Make sure working directory is where the data is saved
+## Analysis script to tranform raw data set to tidy data ready for analysis
+library(data.table)
+library(plyr)
+
+# Make sure working directory is where the data is saved
 datadir <- dirname(sys.frame(1)$ofile)
 if(getwd()!=datadir){
   setwd(datadir)
 }
 
-
-## Get activity lables
+# Parse activity lables
 activity_labels <- scan("UCI-HAR-Dataset/activity_labels.txt", what = "character")
-activity_labels <- activity_labels[seq(2,length(activity_labels), by = 2)] # Get labels only....
+activity_labels <- activity_labels[seq(2,length(activity_labels), by = 2)] # Ignore the numbers in the file....
 
-tbl<- scan("UCI-HAR-Dataset/train/subject_train.txt", what = "numeric", sep = "\n")
+# Parse feature lables
+feature_labels <- scan("UCI-HAR-Dataset/features.txt", what = "character")
+feature_labels <- feature_labels[seq(2,length(feature_labels), by = 2)] # Ignore the numbers in the file....
 
+# Prepare column names for tidy dataset 
+cnames <- c("subject_id","activity_number", "activity_label", feature_labels)
+
+
+## Load in training set first...
+# Read in subject ID, numbered activity & feature vector for that activity
+id_train <- scan("UCI-HAR-Dataset/train/subject_train.txt", what = "numeric", sep = " ")
+activity_numbers_train <- scan("UCI-HAR-Dataset/train/y_train.txt", what = "character", sep = " ")
+activity_labels_train <- mapvalues(activity_numbers_train,c(1,2,3,4,5,6), activity_labels)
+features_train <- scan("UCI-HAR-Dataset/train/X_train.txt", what = "numeric", sep = "\n")
+
+
+
+#tidy_train = data.table(cnames, key = cnames)
+
+## Now deal with testing set
+
+## Combine it all together & save to tidy data folder
