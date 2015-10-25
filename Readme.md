6d9fa225bf7025a8e200a576109273f0ad779314
Cal <cal@tripleanalytics.com>
2015-10-25 04:29:07 -0400

    Transform script complete
    Tidy data set is successfully created

--- Readme.md
+++ Readme.md
@@ -2,13 +2,12 @@
 
 ## Author: Cal Al-Dhubaib
 
-### Included files: run_analysis.R, data_dictionary.csv, Readme.md
+### Included files: run_analysis.R, Readme.md
 ### Included folders: tidy, UCI-HAR-Dataset
 
 The original data files and acompanied readme & descriptions are in the folder UCI-HAR-Dataset.
 
-I ran the R script run-analysis.R to transform the data from the subfolders "train" & "test" in the original dataset to the folder "tidy"
+The script run-analysis.R transforms data from the subfolders "train" & "test" in the original dataset to the folder "tidy". If you check out my repo, and run this script, it will automatically set the working directory to load the data correctly for convenience.
 
-The original dataset was sourced from 
-
+The original dataset was sourced from:
 Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,68 @@
 ## Analysis script to tranform raw data set to tidy data ready for analysis
 library(data.table)
 library(plyr)
+library(dplyr)
 
 # Make sure working directory is where the data is saved
 datadir <- dirname(sys.frame(1)$ofile)
 if(getwd()!=datadir){
   setwd(datadir)
+  print(sprintf("Working directory changed to %s", datadir))
 }
 
-# Parse activity lables
-activity_labels <- scan("UCI-HAR-Dataset/activity_labels.txt", what = "character")
-activity_labels <- activity_labels[seq(2,length(activity_labels), by = 2)] # Ignore the numbers in the file....
+# Label activity lables
+activities <- read.table("UCI-HAR-Dataset/activity_labels.txt")
+colnames(activities) <- c("number", "labels")
 
-# Parse feature lables
-feature_labels <- scan("UCI-HAR-Dataset/features.txt", what = "character")
-feature_labels <- feature_labels[seq(2,length(feature_labels), by = 2)] # Ignore the numbers in the file....
+# Label feature lables
+features <- read.table("UCI-HAR-Dataset/features.txt")
+colnames(features) <- c("number", "label")
+
+# Filter desired features (Only mean & std of each variable)
+desiredFeatures = grepl("mean|std",features$label)
+features <- features[desiredFeatures,]
 
 # Prepare column names for tidy dataset 
-cnames <- c("subject_id","activity_number", "activity_label", feature_labels)
+cnames <- c("subject_id","activity_number", "activity_label", as.character(features$label))
+
+# Build data dictionary 
 
 
-## Load in training set first...
+
+## Load in training set
 # Read in subject ID, numbered activity & feature vector for that activity
-id_train <- scan("UCI-HAR-Dataset/train/subject_train.txt", what = "numeric", sep = " ")
-activity_numbers_train <- scan("UCI-HAR-Dataset/train/y_train.txt", what = "character", sep = " ")
-activity_labels_train <- mapvalues(activity_numbers_train,c(1,2,3,4,5,6), activity_labels)
-features_train <- scan("UCI-HAR-Dataset/train/X_train.txt", what = "numeric", sep = "\n")
+id_train <- read.table("UCI-HAR-Dataset/train/subject_train.txt")
+activity_numbers_train <- read.table("UCI-HAR-Dataset/train/y_train.txt")
+activity_labels_train <- mapvalues(activity_numbers_train$V1,c(1,2,3,4,5,6), as.character(activities$label))
+features_train <- read.table("UCI-HAR-Dataset/train/X_train.txt")
+features_train <- features_train[, desiredFeatures] # Filter for desired feature
 
+print("Training data all loaded...")
 
+# Build the tidy training data set!
+tidy_train = cbind(id_train, activity_numbers_train, activity_labels_train, features_train)
+colnames(tidy_train) <- cnames
 
-#tidy_train = data.table(cnames, key = cnames)
+print("Tidy training data built!")
 
-## Now deal with testing set
+## Load in testing set
+# Read in subject ID, numbered activity & feature vector for that activity
+id_test <- read.table("UCI-HAR-Dataset/test/subject_test.txt")
+activity_numbers_test <- read.table("UCI-HAR-Dataset/test/y_test.txt")
+activity_labels_test <- mapvalues(activity_numbers_test$V1,c(1,2,3,4,5,6), as.character(activities$label))
+features_test <- read.table("UCI-HAR-Dataset/test/X_test.txt")
+features_test <- features_test[, desiredFeatures] # Filter for desired feature
+
+print("Test data all loaded...")
+
+# Build the tidy training data set!
+tidy_test = cbind(id_test, activity_numbers_test, activity_labels_test, features_test)
+colnames(tidy_test) <- cnames
+
+print("Tidy testing data built!")
 
 ## Combine it all together & save to tidy data folder
+print("Combining tidy data sets and exporting to CSV \"tidy_data.csv\"")
+tidy_data <- rbind(tidy_train, tidy_test)
+write.csv(tidy_data,"tidy/tidy_data.csv", header = TRUE, row.names = FALSE)
+print("Export complete")
